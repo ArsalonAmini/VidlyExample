990755e1d08ec5cc07cd7900cc6c1c6d5006f89e
Arsalon <aminihajibasa@gmail.com>
2017-06-25 09:05:50 -0700

    AddedAgeValidationtoCustomerForm

--- VidlyExample/Models/Customer.cs
+++ VidlyExample/Models/Customer.cs
@@ -11,7 +11,7 @@ namespace VidlyExample.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
@@ -24,6 +24,7 @@ namespace VidlyExample.Models
         public byte MembershipTypeId { get; set; }
 
         [Display (Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     
     }
