5a6ad5f403abf0bb55d350243f59dbd0c92a919b
Arsalon <aminihajibasa@gmail.com>
2017-07-07 07:22:06 -0700

    rendering data on client side with customers/api and Jquery bootbox

--- VidlyExample/Controllers/CustomersController.cs
+++ VidlyExample/Controllers/CustomersController.cs
@@ -65,8 +65,7 @@ namespace VidlyExample.Controllers
 
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c =>c.MembershipType).ToList();
-            return View(customers);
+            return View();
         }
 
         public ActionResult Details(int id)
